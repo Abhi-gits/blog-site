b1ece43a054a30edf9e194c954b193c38c0be077
Abhishek Gupta <ravi37463@gmail.com>
2022-08-21 19:08:01 +0530

    host

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-pl+l5rz-h-fyp9@&=p24o&na+escduzg(wbbuq#h9y8&)gym#g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
